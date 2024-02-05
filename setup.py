d956a83969665d5345ffb89cebc57e3619e19dea
AryanRajSaxena <aryanraj052002@gmail.com>
2024-02-05 10:04:43 +0530

    First upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='creating a algorithm which can estimate the change in color at different concentration and at different time instances.',
+    author='Aryan Raj Saxena',
+    license='',
+)
